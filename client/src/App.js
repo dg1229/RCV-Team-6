1443a0c16a54a56a0ae2fa9002458cd5f23819e7
Dylan Gomer <36208962+dg1229@users.noreply.github.com>
2021-10-07 00:30:58 -0400

    Added base ProgressIndicator

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import ProgressIndicator from './components/ProgressIndicator/ProgressIndicator';
 
 function App() {
   const [data, setData] = React.useState(null);
+  
+  const testData = [
+    { bgcolor: "#6a1b9a", completed: 60 },
+    { bgcolor: "#00695c", completed: 30 },
+    { bgcolor: "#ef6c00", completed: 53 },
+  ];
 
   React.useEffect(() => {
     fetch("/api")
@@ -19,6 +26,9 @@ function App() {
         <p>
           {!data ? "Loading..." : data}
         </p>
+        {testData.map((item, idx) => (
+          <ProgressIndicator key={idx} bgcolor={item.bgcolor} completed={item.completed} />
+        ))};
       </header>
     </div>
   );
