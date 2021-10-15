6dd798563af954e76e5899b39058a05419e6deb0
RosareMililani <mr7721@rit.edu>
2021-10-15 10:35:20 -0400

    Added review button, no functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,20 @@ import React from 'react';
 import ProgressIndicator from './components/ProgressIndicator/ProgressIndicator';
 import Ballot from './components/Ballot/Ballot';
 
+
+const reviewButton = {
+  backgroundColor: "#2A64F5",
+  height: "69px",
+  width: "221px",
+  color: "white",
+  border: "none",
+  fontSize: "16pt",
+  fontFamily: '"IBM Plex Mono", monospace',
+  float: "right",
+  marginTop: "2.5%",
+  marginRight: "5%"
+}
+
 function App() {
   const [data, setData] = React.useState(null);
 
@@ -17,7 +31,9 @@ function App() {
     <div className="App">
       <ProgressIndicator completed="50" />
       <Ballot />
-      <footer style={{backgroundColor:'#D6D8D9', height: '150px'}}></footer>
+      <footer style={{backgroundColor:'#D6D8D9', height: '150px'}}>
+        <button style={reviewButton}>REVIEW &#8594;</button>
+      </footer>
     </div>
   );
 }
