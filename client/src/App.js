67285f3f4cb74b367625b7ed48f254296e428249
RosareMililani <mr7721@rit.edu>
2021-10-16 04:31:28 -0400

    Completed page footer for current page, no functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React from 'react';
 import ProgressIndicator from './components/ProgressIndicator/ProgressIndicator';
 import Ballot from './components/Ballot/Ballot';
+import QuestionIcon from './assets/question.png';
+import LanguageIcon from './assets/united-states.png';
 
 
 const reviewButton = {
@@ -10,11 +12,42 @@ const reviewButton = {
   width: "221px",
   color: "white",
   border: "none",
-  fontSize: "16pt",
+  fontSize: "16px",
   fontFamily: '"IBM Plex Mono", monospace',
   float: "right",
   marginTop: "2.5%",
   marginRight: "5%"
+}
+
+const resetChoice = {
+  backgroundColor: "transparent",
+  border: "none",
+  fontSize: "16px",
+  fontFamily: '"IBM Plex Mono", monospace',
+  color: "#2A64F5",
+  float: "right",
+  marginTop: "4%",
+  marginRight: "6%"
+}
+
+const helpButton = {
+  backgroundColor: "transparent",
+  border: "none",
+  fontSize: "16px",
+  fontFamily: '"IBM Plex Mono", monospace',
+  float: "left",
+  marginTop: "4%",
+  marginLeft: "4%"
+}
+
+const languageButton = {
+  backgroundColor: "transparent",
+  border: "none",
+  fontSize: "16px",
+  fontFamily: '"IBM Plex Mono", monospace',
+  float: "left",
+  marginTop: "4%",
+  marginLeft: "5%"
 }
 
 function App() {
@@ -25,14 +58,17 @@ function App() {
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
-  
-  
+
+
   return (
     <div className="App">
       <ProgressIndicator completed="50" />
       <Ballot />
-      <footer style={{backgroundColor:'#D6D8D9', height: '150px'}}>
+      <footer style={{ backgroundColor: '#D6D8D9', height: '150px' }}>
+        <button style={helpButton}><img src={QuestionIcon} alt='question icon'/> HELP</button>
+        <button style={languageButton}><img src={LanguageIcon} alt='language icon'/> ENGLISH</button>
         <button style={reviewButton}>REVIEW &#8594;</button>
+        <button style={resetChoice}>RESET ALL CHOICES</button>
       </footer>
     </div>
   );
