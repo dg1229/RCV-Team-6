eddbfaddddc60f154c0009ec6c311fdbc6ae44bc
SurelyNotChase <chase.lear@outlook.com>
2021-10-17 03:14:08 -0400

    added more ballot features, style improvements

--- client/src/App.js
+++ client/src/App.js
@@ -50,6 +50,41 @@ const languageButton = {
   marginLeft: "5%"
 }
 
+const footerStyle ={ 
+  backgroundColor: '#D6D8D9', 
+  height: '150px',
+  marginBottom: '0px',
+  position: 'absolute',
+  width: '100vw',
+  bottom: '0%',
+  background: '#F4F4F4'
+  
+}
+
+const castButton = {
+  backgroundColor: "#2A64F5",
+  display: "inline-block",
+  height: "69px",
+  width: "221px",
+  color: "white",
+  border: "none",
+  fontSize: "16px",
+  fontFamily: '"IBM Plex Mono", monospace',
+  marginTop: "2.5%",
+}
+
+const editButton = {
+  backgroundColor: "transparent",
+  display: "inline-block",
+  border: "none",
+  fontSize: "16px",
+  fontFamily: '"IBM Plex Mono", monospace',
+  color: "#2A64F5",
+  marginTop: "4%",
+  marginRight: "4%"
+}
+
+
 function App() {
   const [data, setData] = React.useState(null);
 
@@ -62,14 +97,22 @@ function App() {
 
   return (
     <div className="App">
-      <ProgressIndicator completed="50" />
+
+      <ProgressIndicator completed="10" />
       <Ballot />
-      <footer style={{ backgroundColor: '#D6D8D9', height: '150px' }}>
+      
+      <footer style={footerStyle}>
         <button style={helpButton}><img src={QuestionIcon} alt='question icon'/> HELP</button>
         <button style={languageButton}><img src={LanguageIcon} alt='language icon'/> ENGLISH</button>
         <button style={reviewButton}>REVIEW &#8594;</button>
         <button style={resetChoice}>RESET ALL CHOICES</button>
       </footer>
+
+      {/* For Review Page */}
+      {/* <footer style={{height: '150px'}}>
+        <button style={editButton}>RETURN TO EDIT</button>
+        <button style={castButton}>CAST YOUR BALLOT</button>
+      </footer> */}
     </div>
   );
 }
