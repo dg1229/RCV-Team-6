fa9fbbb09b7ed6c41b43ca39cd969b2ffe90ff84
Dylan Gomer <36208962+dg1229@users.noreply.github.com>
2021-10-19 23:49:40 -0400

    Added redirect to home page after vote is confirmed

--- client/src/components/Submit/Submit.js
+++ client/src/components/Submit/Submit.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import { useHistory } from "react-router-dom";
 import Footer from '../Footer/Footer';
 import ProgressIndicator from '../ProgressIndicator/ProgressIndicator';
 
-const centerImage = {
+const centerVideo = {
     display: "block",
     marginLeft: "auto",
     marginRight: "auto",
     marginTop: "10%",
+    textAlign: "center",
     width: "17%"
 }
 
@@ -15,23 +16,27 @@ const mainText = {
     textAlign: "center",
     fontFamily: '"IBM Plex Mono", monospace',
     fontSize: '50px',
-    marginLeft: '2%',
     marginBottom: '0%'
 }
 
 const paraText = {
     textAlign: "center",
-    fontFamily: '"IBM Plex Mono", monospace',
-    marginLeft: '2%'
+    fontFamily: '"IBM Plex Mono", monospace'
 }
 
 
 const Submit = () => {
+
+    const history = useHistory()
+      setTimeout(() => {
+        history.push('/')
+      }, 15000)
+
     return (
         <div >
             <ProgressIndicator completed="100" />
             <div>
-                <video controls>
+                <video autoplay="autoplay" muted="muted" style={centerVideo}>
                   <source src="/Videos/Ballot_Confirmation.mp4" type="video/mp4"></source>
                 </video>
                 <h2 style={mainText}>SUCCESS!</h2>
